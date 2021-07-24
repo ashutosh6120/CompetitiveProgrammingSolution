2d9345687b1f1671dd3b628b8fef00c9722fd33f
ashutosh6120 <ashutosh4064@gmail.com>
2021-07-24 21:47:57 +0530

    day 1 prgm added

---
+++ HackerRank/30 days of Code/Day-1-Data Types.py
@@ -0,0 +1,15 @@
+
+a = int(raw_input())
+b = float(raw_input())
+c = str(raw_input())
+
+# Print the sum of both integer variables on a new line.
+print i + a
+
+# Print the sum of the double variables on a new line.
+print format(d + b, '.1f')
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + c
+
