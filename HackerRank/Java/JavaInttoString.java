9d9d94f0ca5d9e23c227d0e5c759d7e379fd19a6
ashutosh6120 <ashutosh4064@gmail.com>
2021-08-24 14:25:30 +0530

    prgm added

---
+++ HackerRank/Java/JavaInttoString.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+
+public class JavaInttoString {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //String s;
+        String s = Integer.toString(n);
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }    
+    }
+}
