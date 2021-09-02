9d299db4ba49c7cb4a6a0f6cc4e5dcc435976f9b
ashutosh6120 <ashutosh4064@gmail.com>
2021-09-03 00:17:35 +0530

    life the universe and everything prgm added

---
+++ Codechef/Life-The Universe and Everything.c
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/TEST
+//Author - Ashutosh Dodamani, SJCE Mysore
+//@ ashutosh6120
+
+int main()
+{	int x;
+	scanf("%d",&x);
+    if((x<100)&&(x!=42))
+    {
+		printf("%d \n",x);
+		return main();
+	}
+    return 0;
+}
