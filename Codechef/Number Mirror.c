0ce4e9b2dbf65bb9e94baf3111af3eaaa15aa544
ashutosh6120 <ashutosh4064@gmail.com>
2021-09-04 19:31:20 +0530

    number mirror in C

---
+++ Codechef/Number Mirror.c
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/START01
+//Author - Ashutosh Dodamani, SJCE Mysore
+//@ ashutosh6120
+
+#include <stdio.h>
+int main() {
+	// your code goes here
+	char a[100];
+	scanf("%s",a);
+	printf("%s",a);
+}
