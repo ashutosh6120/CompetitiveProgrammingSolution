b1d3e9af24195ec321456688140c49e8852137f6
ashutosh6120 <ashutosh4064@gmail.com>
2021-09-04 19:32:30 +0530

    number mirror in cpp

---
+++ Codechef/Number Mirror.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/YTPP001/problems/START01
+//Author - Ashutosh Dodamani, SJCE Mysore
+//@ ashutosh6120
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	cout << n << endl;
+	return 0;
+}
