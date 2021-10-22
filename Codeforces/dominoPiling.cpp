9f1dc3bf974fe9d788b076ffe465cfe24a007408
ashutosh6120 <ashutosh4064@gmail.com>
2021-10-22 08:01:22 +0530

    domino pilling prgm added

---
+++ Codeforces/dominoPiling.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/contest/50/problem/A
+//Author - Ashutosh Dodamani, SJCE Mysore
+//@ ashutosh6120
+
+
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main()
+{
+    int n,m,res;
+    cin >> m >> n;
+    res = n * m / 2;
+    cout << res << endl;
+}
