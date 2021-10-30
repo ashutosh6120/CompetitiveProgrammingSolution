44fd48123b320a7a92242cf513b736915be0d048
ashutosh6120 <ashutosh4064@gmail.com>
2021-10-30 13:45:11 +0530

    day 5 prgm added

---
+++ HackerRank/30 days of Code/Day-5-Loops.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i < 11; i++) {
+        printf("%i x %i = %i\n", N, i, N * i);
+    }
+    return 0;
+}
+
