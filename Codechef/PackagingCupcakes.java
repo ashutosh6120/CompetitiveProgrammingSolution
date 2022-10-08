4962564ab2ec202ed38ba687250768edb58280eb
ashutosh6120 <ashutosh4064@gmail.com>
2022-10-08 14:01:29 +0530

    packaging cupcakes soln added

---
+++ Codechef/PackagingCupcakes.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PackagingCupcakes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    System.out.println((n/2)+1);
+		}
+        sc.close();
+	}
+}
