2cfe00b25c91358305c52665978bea19ca528a03
ashutosh6120 <ashutosh4064@gmail.com>
2022-10-18 19:51:43 +0530

    sum pr diff soln added

---
+++ Codechef/Sumordifference.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Sumordifference
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		if(n1>n2){
+		    System.out.println(n1-n2);
+		}else{
+		    System.out.println(n1+n2);
+		}
+        sc.close();
+	}
+}
