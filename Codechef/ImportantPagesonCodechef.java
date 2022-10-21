91042186b42aa136e12fb35629f87cbd291c16e1
ashutosh6120 <ashutosh4064@gmail.com>
2022-10-21 22:35:06 +0530

    imp pages on codechef soln added

---
+++ Codechef/ImportantPagesonCodechef.java
@@ -0,0 +1,28 @@
+
+
+
+import java.util.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ImportantPagesonCodechef 
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a==0){
+		    System.out.println("https://www.codechef.com/practice");
+		}
+		else if(a==1 && b==0){
+		    System.out.println("https://www.codechef.com/contests");
+		}
+		else if(a==1 && b==1){
+		    System.out.println("https://discuss.codechef.com");
+		}
+		sc.close();
+	}
+}
+
