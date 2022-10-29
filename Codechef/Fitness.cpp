5f012f73b49dc020bf67e4106ab4a4b34e9de8b1
ashutosh6120 <ashutosh4064@gmail.com>
2022-10-29 18:56:39 +0530

    fitness soln added

---
+++ Codechef/Fitness.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    int res = (x*2) * 5;
+	    std::cout << res << std::endl;
+	}
+	return 0;
+}
