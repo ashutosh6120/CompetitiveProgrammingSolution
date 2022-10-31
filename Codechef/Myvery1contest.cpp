beb578feec607e79f9a27f664b468861ac3f0277
ashutosh6120 <ashutosh4064@gmail.com>
2022-10-31 14:22:31 +0530

    my very 1 contest soln added

---
+++ Codechef/Myvery1contest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	cin >> n >> a >> b;
+	int val = n-a;
+	std::cout << val << " ";
+    std::cout << val-b << std::endl;
+	return 0;
+}
