247da3853d5a2137411ba9db473eae1bb579a6bd
ashutosh6120 <ashutosh4064@gmail.com>
2022-10-31 14:21:38 +0530

    reach the target soln added

---
+++ Codechef/Reachthetarget.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int y,x;
+	    cin >> y >> x;
+	    std::cout << y-x << std::endl;
+	}
+	return 0;
+}
